e414fabd8b248bc3e05e9393856a99c00f303670
Parker Selbert <parker@sorentwo.com>
2020-11-29 22:53:37 -0500

    Oban loop timing and locking refinement
    The entire oban_loop call is wrapped in an advisory lock to reduce
    connection holding.

--- sql/oban_loop.sql
+++ sql/oban_loop.sql
@@ -1,18 +1,30 @@
+/*
+ * Start the core oban loop, which runs plugin procedures in an infinite timed loop.
+ *
+ * Plugins are specified as `name` => `opts` pairs, where the name is a loaded procedure and the
+ * opts are a JSON object. The structure of opts is flexible and plugin dependent, except for a
+ * standard `interval` option. Intervals are specified in seconds, which determines how frequently
+ * a plugin is invoked, at most once per second.
+*/
 create or replace function oban_loop(conf jsonb) returns void as $func$
 declare
   name text;
   opts json;
+  iter int := 0;
   lkey bigint := 1235711000000000000;
 begin
-  loop
-    if pg_try_advisory_lock(lkey) then
-      -- some plugins only run every minute, right? do we need that for cron?
-      for name, opts in select * from jsonb_each(conf->'plugins') loop
-        execute format('call %i($1)', name) using opts;
-      end loop;
+  if not pg_try_advisory_lock(lkey) then
+    raise info 'oban loop for % already running', lkey;
+  end if;
 
-      perform pg_advisory_unlock(lkey);
-    end if;
+  loop
+    for name, opts in select * from jsonb_each(conf->'plugins') loop
+      if mod(iter, coalesce((opts->>'interval')::int, 1)) = 0 then
+        execute format('call %I($1)', name) using opts;
+      end if;
+    end loop;
+
+    iter := iter + 1;
 
     perform pg_sleep(1);
   end loop;
