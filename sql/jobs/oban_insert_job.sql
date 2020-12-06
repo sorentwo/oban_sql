1c0947f6b65734f55582e12284f089da618dbfa8
Parker Selbert <parker@sorentwo.com>
2020-12-05 23:31:16 -0500

    Round out unique support for inserts
    The query function is fully functional, but it still lacks advisory
    locking.

--- sql/jobs/oban_insert_job.sql
+++ sql/jobs/oban_insert_job.sql
@@ -1,12 +1,80 @@
--- %{fields: fields, keys: keys, period: period, states: states} = unique
--- abs(('x' || md5('what-the-heck-now'))::bit(64)::bigint)
--- build up the query
-  -- period
-  -- states
-  -- worker?
-  -- queue?
-  -- args? (maybe by keys)
--- compute a lock
+create or replace function oban_unique_fetch(queue text, worker text, args jsonb, meta jsonb)
+returns oban_jobs as $func$
+declare
+  query text;
+  job oban_jobs;
+  keys jsonb;
+  fields jsonb;
+  period jsonb;
+  states jsonb;
+  default_fields jsonb := '["args", "queue", "worker"]';
+  default_period jsonb := '60';
+  default_states jsonb := '["scheduled", "available", "executing", "retryable", "completed"]';
+begin
+  if meta ? 'unique' then
+    keys := coalesce(meta #> '{unique,keys}', '[]');
+    fields := coalesce(meta #> '{unique,fields}', default_fields);
+    period := coalesce(meta #> '{unique,period}', default_period);
+    states := coalesce(meta #> '{unique,states}', default_states);
+
+    query := $_$ select * from oban_jobs where true $_$;
+
+    query := query || format(
+      $_$ and inserted_at > utc_now() - '%s seconds'::interval $_$,
+      period
+    );
+
+    query := query || format(
+      $_$ and state = any (select elem::oban_job_state from jsonb_array_elements_text('%s') elem) $_$,
+      states
+    );
+
+    if jsonb_array_length(keys) > 0 then
+      select jsonb_object_agg(key, value) from jsonb_each(args) where keys ? key into args;
+    end if;
+
+    if fields ? 'args' then
+      query := query || format($_$ and args @> '%s' $_$, args);
+    end if;
+
+    if fields ? 'queue' then
+      query := query || format($_$ and queue = '%s' $_$, queue);
+    end if;
+
+    if fields ? 'worker' then
+      query := query || format($_$ and worker = '%s' $_$, worker);
+    end if;
+
+    query := query || $_$ order by id limit 1 $_$;
+
+    execute query into job;
+
+    return job;
+  else
+    return null;
+  end if;
+end $func$
+language plpgsql
+set search_path from current;
+
+--
+-- Dynamic uniqueness doesn't rely on an index. To prevent concurrent inserts we use an advisory
+-- lock around the uniqueness scan and job insertion. The lock is composed from the queue, worker,
+-- and args (only some args if `keys` is provided).
+--
+create or replace function oban_unique_lock(queue text, worker text, args jsonb, meta jsonb)
+returns boolean as $func$
+declare
+begin
+  if meta ? 'unique' then
+    -- abs(('x' || md5('what-the-heck-now'))::bit(64)::bigint)
+    return pg_try_advisory_xact_lock(1::bigint);
+  else
+    return true;
+  end if;
+end $func$
+language plpgsql
+set search_path from current;
 
 create or replace function oban_insert_job(
   queue text,
@@ -30,26 +98,23 @@ begin
     state := 'scheduled';
   end if;
 
-  if meta ? 'unique' then
-    unique_query := oban_unique_query(queue, worker, args, meta->'unique');
-    unique_lkey := oban_unique_lkey(queue, worker, args, meta->'unique');
-  else
-    unique_query := 'select true';
-    unique_lkey := trunc(txid_current() * random() * 10000000)::bigint;
-  end if;
+  if oban_unique_lock(queue, worker, args, meta) then
+    job := oban_unique_fetch(queue, worker, args, meta);
 
-  -- and execute unique_query
+    if job.id is not null then
+      return job;
+    end if;
 
-  if pg_try_advisory_xact_lock(unique_lkey) then
     insert into oban_jobs (queue, worker, args, meta, priority, max_attempts, state, scheduled_at)
     values (queue, worker, args, meta, priority, max_attempts, state, scheduled_at)
     on conflict do nothing
     returning *
     into job;
-  end if;
 
-  if job is not null then
-    perform oban_notify('oban_insert', json_build_object('queue', job.queue));
+    -- Only notify if this is a new job, not when it was returned from on conflict
+    if job.inserted_at >= utc_now() then
+      perform oban_notify('oban_insert', json_build_object('queue', job.queue));
+    end if;
   end if;
 
   return job;
--- test/jobs/oban_insert_job_test.sql
+++ test/jobs/oban_insert_job_test.sql
@@ -12,5 +12,137 @@ begin
     array['alpha', 'gamma'],
     'jobs are inserted'
   );
-end;
+end
+$func$ language plpgsql;
+
+create or replace function oban_unique_fields_test()
+returns setof text as $func$
+begin
+  insert into oban_jobs (id, queue, worker, args) values
+    (100, 'alpha', 'Worker.A', '{"id": 1}'),
+    (101, 'gamma', 'Worker.A', '{"id": 2}'),
+    (102, 'delta', 'Worker.B', '{"id": 3}');
+
+  return next set_eq(
+    $$ select id from oban_insert_job('alpha', 'Worker.B', '{}', '{"unique": {"fields": ["queue"]}}') $$,
+    array[100],
+    'uniqueness is enforced by queue'
+  );
+
+  return next set_eq(
+    $$ select id from oban_insert_job('alpha', 'Worker.B', '{}', '{"unique": {"fields": ["worker"]}}') $$,
+    array[102],
+    'uniqueness is enforced by worker'
+  );
+
+  return next set_eq(
+    $$ select id from oban_insert_job('gamma', 'Worker.A', '{}', '{"unique": {"fields": ["queue", "worker"]}}') $$,
+    array[101],
+    'uniqueness is enforced by queue and worker'
+  );
+
+  return next set_ne(
+    $$ select id from oban_insert_job('delta', 'Worker.A', '{}', '{"unique": {"fields": ["queue", "worker"]}}') $$,
+    array[101],
+    'insert is allowed with mismatched queue and worker'
+  );
+
+  return next set_eq(
+    $$ select id from oban_insert_job('alpha', 'Worker.A', '{"id": 1}', '{"unique": {"fields": ["args"]}}') $$,
+    array[100],
+    'uniqueness is enforced by args'
+  );
+
+  return next set_eq(
+    $$ select id from oban_insert_job('alpha', 'Worker.A', '{"id": 1}', '{"unique": {"fields": ["args", "worker"]}}') $$,
+    array[100],
+    'uniqueness is enforced by worker and args'
+  );
+
+  return next set_ne(
+    $$ select id from oban_insert_job('alpha', 'Worker.A', '{"id": 2}', '{"unique": {"fields": ["args", "worker"]}}') $$,
+    array[100],
+    'insert is allowed with mismatched worker and args'
+  );
+end
+$func$ language plpgsql;
+
+create or replace function oban_unique_fields_test()
+returns setof text as $func$
+begin
+  insert into oban_jobs (id, queue, worker, args) values
+    (100, 'alpha', 'Worker.A', '{"id": 1, "url": "a.co"}'),
+    (101, 'alpha', 'Worker.A', '{"id": 2, "url": "b.co"}');
+
+  return next set_eq(
+    $$ select id from oban_insert_job(
+      'alpha', 'Worker.A', '{"id": 1, "url": "c.co"}', '{"unique": {"keys": ["id"]}}')
+    $$,
+    array[100],
+    'uniqueness is enforced by only id key'
+  );
+
+  return next set_eq(
+    $$ select id from oban_insert_job(
+      'alpha', 'Worker.A', '{"id": 3, "url": "a.co"}', '{"unique": {"keys": ["url"]}}')
+    $$,
+    array[100],
+    'uniqueness is enforced by only url key'
+  );
+
+  return next set_ne(
+    $$ select id from oban_insert_job(
+      'alpha', 'Worker.A', '{"id": 2, "url": "c.co"}', '{"unique": {"keys": ["url"]}}')
+    $$,
+    array[101],
+    'insert is allowed with a single mismatched key'
+  );
+end
+$func$ language plpgsql;
+
+create or replace function oban_unique_states_test()
+returns setof text as $func$
+begin
+  insert into oban_jobs (id, queue, worker, state) values
+    (100, 'alpha', 'Worker.A', 'available'),
+    (101, 'alpha', 'Worker.B', 'completed'),
+    (102, 'alpha', 'Worker.C', 'executing');
+
+  return next set_eq(
+    $$ select id from oban_insert_job(
+      'alpha', 'Worker.A', '{}', '{"unique": {"fields": ["worker"], "states": ["available"]}}')
+    $$,
+    array[100],
+    'uniqueness is enforced by available state'
+  );
+
+  return next set_ne(
+    $$ select id from oban_insert_job(
+      'alpha', 'Worker.A', '{}', '{"unique": {"fields": ["worker"], "states": ["completed"]}}')
+    $$,
+    array[100],
+    'insert is allowed when state does not match'
+  );
+end
+$func$ language plpgsql;
+
+create or replace function oban_unique_period_test()
+returns setof text as $func$
+begin
+  insert into oban_jobs (id, queue, worker, inserted_at) values
+    (100, 'alpha', 'Worker.A', utc_now() - '1 minute'::interval),
+    (101, 'alpha', 'Worker.B', utc_now() - '1 year'::interval);
+
+  return next set_eq(
+    $$ select id from oban_insert_job('alpha', 'Worker.A', '{}', '{"unique": {"period": 61}}') $$,
+    array[100],
+    'uniqueness is enforced within a period'
+  );
+
+  return next set_ne(
+    $$ select id from oban_insert_job('alpha', 'Worker.A', '{}', '{"unique": {"period": 59}}') $$,
+    array[100],
+    'insert is allowed after the period'
+  );
+end
 $func$ language plpgsql;
